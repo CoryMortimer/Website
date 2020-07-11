01bd722b0483fb11842df66dba5d9b271cc3ade3
Cory Mortimer <cory.m.mortimer@gmail.com>
2020-07-10 21:53:32 -0700

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,6 +64,13 @@ module.exports = {
         icon: `src/images/face-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-112016091-2',
+        head: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
